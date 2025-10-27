23c7c1bf07e1ce2efa658317a3d3fda26b9b7478
quangvinh263 <23521792@gm.uit.edu.vn>
2025-10-27 21:10:51 +0700

    Implement RoomsPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import './App.css'
 import HomePage from './pages/HomePage'
 import SignInPage from './pages/SignInPage'
 import SignUpPage from './pages/SignUpPage'
+import RoomsPage from './pages/RoomsPage'
 
 function App() {
   return (
-      <HomePage />
+      <RoomsPage />
   )
 }
 export default App
